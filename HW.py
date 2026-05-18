8188d4121eb8445fa67e0463c402a0591656fd2f
DishaSingal <aashwanisingal@gmail.com>
2026-05-18 12:02:26 +0530

    Add files via upload

---
+++ HW.py
@@ -0,0 +1,6 @@
+attendence = int(input("Enter your exam percentage"))
+
+if attendence >=75:
+    print("You are eligible to give the exam!")
+else:
+    print(" You are not eligible. you need 755 attendence to br eligible to give the exam")
